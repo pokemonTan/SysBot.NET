0a649245a910b7327694a2b6e798521b0d1a47c0
havshark <havshark@qq.com>
2025-08-01 00:35:11 +0800

    修改

--- NapCatScript.Core/CoreConfigValueAndObject.cs
+++ NapCatScript.Core/CoreConfigValueAndObject.cs
@@ -26,8 +26,11 @@ public static class CoreConfigValueAndObject
         //string? useUri = "ws://39.108.178.212:3001/?access_token=G5.Zk.IqX.";
         //string? httpUri = "http://39.108.178.212:3000/";
         //文件服务器
-        string? useUri = "ws://14.152.49.93:3001/?access_token=G5.Zk.IqX.";
-        string? httpUri = "http://14.152.49.93:3000/";
+        //string? useUri = "ws://14.152.49.93:3001/?access_token=G5.Zk.IqX.";
+        //string? httpUri = "http://14.152.49.93:3000/";
+        //线上数据库服务器
+        string? useUri = "ws://120.48.195.144:3001/?access_token=G5.Zk.IqX.";
+        string? httpUri = "http://120.48.195.144:3000/";
         string? httpAccessToken = "p2?4UpMx9r";
         BotId = "3861914692";
         if (string.IsNullOrEmpty(useUri) || string.IsNullOrEmpty(httpUri)) {
